e02fec985c3e53464c1f94b8eabc8a61b40ea71d
kimsiyeon <pennya6@naver.com>
2021-09-30 23:42:18 +0900

    react update

--- chatbot-react/src/main/jsx/main.jsx
+++ chatbot-react/src/main/jsx/main.jsx
@@ -1,3 +1,10 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
 
 const Main=()=>{
@@ -10,7 +17,26 @@ const Main=()=>{
                     <button>ChatBot</button>
                 </Link>
 
+                <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                    Fall in chatbot
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                    adjective
+                </Typography>
+                <Typography variant="body2">
+                    well meaning and kindly.
+                    <br />
+                    {'"a benevolent smile"'}
+                </Typography>
+                </CardContent>
+                <CardActions>
+                <Button size="small">Learn More</Button>
+                </CardActions>
+            </Card>
         </div>
+        
     );
 };
 
